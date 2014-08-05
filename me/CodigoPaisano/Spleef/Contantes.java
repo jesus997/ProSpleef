b947bffa145ee11dea46d01ae3e4b5293be4dd60
jesus997 <jesus9711@live.com.mx>
2014-08-04 21:07:13 -0500

    Create Contantes.java

---
+++ me/CodigoPaisano/Spleef/Contantes.java
@@ -0,0 +1,7 @@
+package me.CodigoPaisano.Spleef;
+
+public class Contantes {
+    public static enum Estado{
+        Iniciando, EnCurso, Finalizada, NoDisponible, EsperandoJugadores, SinIniciar, Regenerando
+    }
+}
