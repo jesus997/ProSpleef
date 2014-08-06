6c1958dc4a349124ee896e440331e558bd4e26a2
jesus997 <jesus9711@live.com.mx>
2014-08-06 11:49:28 -0500

    Update ArenaManager.java

--- me/CodigoPaisano/Spleef/ArenaManager/ArenaManager.java
+++ me/CodigoPaisano/Spleef/ArenaManager/ArenaManager.java
@@ -6,7 +6,9 @@ import me.CodigoPaisano.Spleef.Util.UPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -18,10 +20,15 @@ import org.bukkit.potion.PotionEffect;
  * @author jesus
  */
 public class ArenaManager {
-    private static ArenaManager am = new ArenaManager();
+    private Spleef main;
+    public ArenaManager(Spleef main){
+        this.main = main;
+    }
+    
+    /*private static ArenaManager am = new ArenaManager();
     public static ArenaManager getManager(){
         return am;
-    }
+    }*/
     
     public Arena getArena(String arena){
         for(Arena a : Arena.arenaObjects){
@@ -48,8 +55,17 @@ public class ArenaManager {
            
            for(String s : a.getPlayers()){
                Player p = Bukkit.getPlayer(s);
-               //removePlayer(p);
+               removePlayers(p);
            }
+        }
+    }
+    
+    public void removeArena(Player p, String arena){
+        if(getArena(arena) != null){
+            Arena a = getArena(arena);
+            Arena.arenaObjects.remove(a);
+            // Eliminarla de la config
+            p.sendMessage(getColor("&2Arena eliminada!"));
         }
     }
     
@@ -85,10 +101,7 @@ public class ArenaManager {
         for(Arena a : Arena.arenaObjects){
             if(a.getPlayers().contains(p.getName())){
                 removeItems(p);
-                
-                if(a.getSpectator().contains(p.getName())){
-                    //Remover jugador de espectador
-                }
+                removeSpectator(p, a);
                 
                 p.teleport(a.getEndLocation());
                 UPlayer.getUPlayer(p).exitPlayer();
@@ -108,9 +121,18 @@ public class ArenaManager {
             p.setGameMode(GameMode.ADVENTURE);
             p.setFlying(true);
             
-            // Estas como espectador.
-        }else{
-            // Ya eres un espectador!
+            p.sendMessage(getColor("&2Has sido movido al grupo &b&lSpectador"));
+        }
+    }
+    
+    public void removeSpectator(Player p, Arena a){
+        if(a.getSpectator().contains(p.getName())){
+           p.setFlying(false);
+           p.setGameMode(GameMode.SURVIVAL);
+           Spleef.spec.removePlayer(p);
+           a.getSpectator().remove(p.getName());
+           
+           p.sendMessage(getColor("&2Has sido removido del grupo &b&lSpectador"));
         }
     }
     
@@ -146,6 +168,59 @@ public class ArenaManager {
     }
     
     public String getColor(String str){
-        return ChatColor.translateAlternateColorCodes('&', str);
+        return ChatColor.translateAlternateColorCodes('&', "&a[&f&lSpleef&a]" + str);
+    }
+    
+    public void loadArenas(Spleef m){
+        FileConfiguration fc = m.getArenaConfig();
+        for(String name : fc.getConfigurationSection("Arenas").getKeys(false)){
+            Location joinLocation = deserializeLoc(fc.getString("Arenas." + name + "joinLocation"));
+            Location endLocation = deserializeLoc(fc.getString("Arenas." + name + "endLocation"));
+            int minPlayers = fc.getInt("Arenas." + name + "minPlayers");
+            int maxPlayers = fc.getInt("Arenas." + name + "maxPlayers");
+            Arena a = new Arena(name, joinLocation, endLocation, minPlayers, maxPlayers);
+            for(String s : fc.getConfigurationSection("spawns").getKeys(false)){
+                a.addSpawnsPoint(deserializeLoc(s));
+            }
+        }
+    }
+    
+    public void createArena(String name, int minPlayers, int maxPlayers){
+        Arena a = new Arena(name, null, null, minPlayers, maxPlayers);
+        main.ConfigCreateArena(name, minPlayers, maxPlayers);
+    }
+    
+    public void createJoinLocation(Location l, String arena){
+        if(getArena(arena) != null){
+            Arena a = getArena(arena);
+            a.setJoinLocation(l);
+            main.ConfigAddJoinLocation(arena, l);
+        }
+    }
+
+    public void createEndLocation(Location l, String arena){
+        if(getArena(arena) != null){
+            Arena a = getArena(arena);
+            a.setEndLocation(l);
+            main.ConfigAddEndLocation(arena, l);
+        }
+    }
+    
+    public void createSpawns(Location l, String arena, Player p){
+        if(getArena(arena) != null){
+            Arena a = getArena(arena);
+            a.addSpawnsPoint(l); 
+            main.ConfigAddSpawnsPont(a.getName(), l, a.getSpawnsPoint().size());
+            p.sendMessage("&2Respawn &b&l" + a.getSpawnsPoint().size() + " &2creado para la arena &a&l" + a.getName());
+        }
+    }
+    
+    public String serializeLoc(Location l){
+        return l.getWorld().getName()+","+l.getBlockX()+","+l.getBlockY()+","+l.getBlockZ(); 
+    }
+    
+    public Location deserializeLoc(String s){
+        String[] st = s.split(",");
+        return new Location(Bukkit.getWorld(st[0]), Double.parseDouble(st[1]), Double.parseDouble(st[2]), Double.parseDouble(st[3]));
     }
 }
